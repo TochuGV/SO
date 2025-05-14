ddbf71c121e3a8c380af7d8e0a91cd0a3180f51e
Valentín Fernández Pizzella <val.fernandez2871@gmail.com>
2025-05-14 20:50:26 +0000

    Update main.c

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -9,24 +9,41 @@ int main(int argc, char* argv[])
 
   int32_t identificador_cpu = atoi(argv[1]);
 
-  //int conexion_kernel_dispatch = crear_conexion(ip_kernel, puerto_kernel_dispatch, CPU);
-  //int conexion_kernel_interrupt = crear_conexion(ip_kernel, puerto_kernel_interrupt, CPU);
-  //int conexion_memoria = crear_conexion(ip_memoria, puerto_memoria, CPU);
+  //IPs
+  char* ip_kernel = config_get_string_value(config, "IP_KERNEL");
+  char* ip_memoria = config_get_string_value(config, "IP_MEMORIA");
 
-  t_cpu_args* args = malloc(sizeof(t_cpu_args));
+  //Puertos
+  char* puerto_kernel_dispatch = config_get_string_value(config, "PUERTO_KERNEL_DISPATCH");
+  char* puerto_kernel_interrupt = config_get_string_value(config, "PUERTO_KERNEL_INTERRUPT");
+  char* puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
 
-  pthread_t cpu_thread;
-  pthread_create(&cpu_thread, NULL, conexiones_modulos, (void*) args);
+  //Hilos
+  pthread_t hilo_conexion_kernel_dispatch;
+  pthread_t hilo_conexion_kernel_interrupt;
+  pthread_t hilo_conexion_memoria;
 
+  //Creación de hilos
+  pthread_create(&hilo_conexion_kernel_dispatch, NULL, conectar_kernel_dispatch, ip_kernel);
+  pthread_create(&hilo_conexion_kernel_interrupt, NULL, conectar_kernel_interrupt, ip_kernel);
+  pthread_create(&hilo_conexion_memoria, NULL, conectar_memoria, ip_memoria);
 
-  if (handshake_cpu(identificador_cpu, args->socket_dispatch) == 0) {
-    paquete(args->socket_dispatch);
-  }
+  //Creación de conexiones
+  int conexion_kernel_dispatch= crear_conexion(ip_kernel, puerto_kernel_dispatch, CPU);
+  int conexion_kernel_interrupt= crear_conexion(ip_kernel, puerto_kernel_interrupt, CPU);
+  int conexion_memoria = crear_conexion(ip_memoria, puerto_memoria, CPU);
+  
+  pthread_join(conexion_kernel_dispatch, NULL);
+  pthread_join(conexion_kernel_interrupt, NULL);
+  pthread_join(conexion_memoria, NULL);
 
-  void* dispatch = manejar_dispatch(args);
+  terminar_programa(conexion_memoria, logger, config);
+
+  void* dispatch = manejar_dispatch(conexion_kernel_dispatch,conexion_memoria);
   
   //terminar_programa(conexion, logger, config);
   free (dispatch);
 
   return EXIT_SUCCESS;
 }
+
