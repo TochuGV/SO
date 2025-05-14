edea0332194f327189256c4623745bcb466ac5e8
Valentín Fernández Pizzella <val.fernandez2871@gmail.com>
2025-05-14 20:49:55 +0000

    Update common_cpu.h

--- cpu/src/common_cpu.h
+++ cpu/src/common_cpu.h
@@ -3,27 +3,22 @@
 
 #include "./utils/utils.h"
 
-typedef struct {
-    char* ip_kernel;
-    char* ip_memoria;
-    char* puerto_kernel_dispatch;
-    char* puerto_kernel_interrupt;
-    char* puerto_memoria;
-    int socket_dispatch;
-    int socket_interrupt;
-    int socket_memoria;
-} t_cpu_args;
+void* atender_cpu_dispatch(void* arg);
+void* atender_cpu_interrupt(void* arg);
+void* atender_memoria(void* arg);
 
-void* conexiones_modulos(void* arg);
+void* conectar_kernel_dispatch(void* arg);
+void* conectar_kernel_interrupt(void* arg);
+void* conectar_memoria(void* arg);
 
-int32_t handshake_cpu(int, int);
+int32_t handshake_kernel_dispatch(int32_t, int);
+int32_t handshake_kernel_interrupt(int32_t, int);
 int32_t handshake_memoria(int32_t, int);
 
-void* manejar_dispatch(t_cpu_args*);
+void* manejar_dispatch(int, int);
 t_pcb* recibir_pcb(int);
 t_pcb* deserializar_pcb(void*);
-void* serializar_pcb(t_pcb*);
-void* enviar_pc_a_memoria(t_pcb*, int);
+void* recibir_instruccion(t_pcb*, int);
 //void* ciclo_de_instruccion(pcb);
 
 #endif /* COMMON_CPU_H_ */
