e0be80fc9a672cf4e8bb74b0c30edd4401a1f49c
Valentín Fernández Pizzella <val.fernandez2871@gmail.com>
2025-05-19 17:08:02 -0300

    Iniciar_cpu,recibir_instruccion y avances en trabajar_instrucción
    1) Iniciar_cpu ahora recibe como parámetro identificador_cpu para luego poderselo mandar a memoria a través de datos_memoria
    2) recibir_instruccion ahora recibe un dato de tipo t_list* para coincidir con lo que manda memoria. Su respectivo tratado y transformación a t_instrucción tambien está hecho
    3) decode ahora es trabajar_instruccion, la idea es aprovechar el switch para que una vez que detecte el caso, directamente lo ejecute ahí adentro y devuelva lo que haga falta

--- cpu/src/common_cpu.c
+++ cpu/src/common_cpu.c
@@ -9,7 +9,7 @@
  datos_conexion_t* datos_interrupt;
  datos_conexion_t* datos_memoria;
 
-void iniciar_cpu() {
+void iniciar_cpu(int32_t identificador_cpu) {
 
   ip_kernel = config_get_string_value(config, "IP_KERNEL");
   ip_memoria = config_get_string_value(config, "IP_MEMORIA");
@@ -21,14 +21,17 @@ void iniciar_cpu() {
   datos_dispatch = malloc(sizeof(datos_conexion_t));
   datos_dispatch->ip = ip_kernel;
   datos_dispatch->puerto = puerto_kernel_dispatch;
+  datos_dispatch->id_cpu = identificador_cpu;
 
   datos_interrupt = malloc(sizeof(datos_conexion_t));
   datos_interrupt->ip = ip_kernel;
   datos_interrupt->puerto = puerto_kernel_interrupt;
-  
+  datos_interrupt->id_cpu = identificador_cpu;
+
   datos_memoria = malloc(sizeof(datos_conexion_t));
   datos_memoria->ip = ip_memoria;
   datos_memoria->puerto = puerto_memoria;
+  datos_memoria->id_cpu = identificador_cpu;
 }
 
 //Conexiones
@@ -42,8 +45,10 @@ void* conectar(void* arg) {
 
     int32_t handshake_header = CPU;
     int32_t respuesta;
+    int32_t identificador = datos->id_cpu;
     send(socket, &handshake_header, sizeof(int32_t), 0);
     recv(socket, &respuesta, sizeof(int32_t), MSG_WAITALL);
+    send(socket, &identificador, sizeof(int32_t), 0);
 
     if (respuesta <= 0) {
       log_error(logger, "Fallo al recibir respuesta del handshake");
@@ -58,31 +63,46 @@ void* conectar(void* arg) {
     return NULL;
 }
 
-void* manejar_dispatch(int conexion_kernel_dispatch,int conexion_memoria) {
-  // Paso 1: recibir el PCB desde kernel
-  t_pcb* pcb = recibir_pcb(conexion_kernel_dispatch);
+void* ciclo_de_instruccion(int conexion_kernel_dispatch,int conexion_memoria) {
+  t_pcb* pcb; 
+  t_list* lista_instruccion;
+  t_instruccion instruccion;
+  t_resultado_ejecucion estado;
+  // Paso 1: recibir el PCB desde Kernel
+  pcb = recibir_pcb(conexion_kernel_dispatch);
   if (pcb == NULL) return NULL;
-  
-  // Paso 2: obtener la dirección de memoria de la instrucción
-   t_instruccion instruccion;
-   
-   recibir_instruccion(pcb, conexion_memoria);
-  // Paso 3: ejecutar instrucción
-  decode(instruccion,pcb->pid);
-  //ciclo_de_instruccion(pcb);
 
-  // Paso 5: liberar memoria (si es necesario)
-  free(pcb);
- 
+  while(1){
+    //Log Fetch instrucción
+    log_info (logger, "## PID: %d - FETCH - Program Counter: %d", pcb->pid,pcb->pc);
+    
+    // Paso 2: obtener la instrucción desde Memoria
+    lista_instruccion = recibir_instruccion(pcb, conexion_memoria);
+    instruccion->tipo = *(t_instruccion*)list_get(lista_instruccion, 0);
+    instruccion->parametro1 = *(t_instruccion*)list_get(lista_instruccion, 1);
+    instruccion->parametro2 = *(t_instruccion*)list_get(lista_instruccion, 2);
+
+    list_destroy_and_destroy_elements(lista_instruccion,free);
+    // Paso 3: interpretar y ejecutar instrucción
+    estado = trabajar_instruccion(instruccion,pcb);
+    if (estado!=EJECUCION_CONTINUA){
+      break;
+    }
+  }
+  // Paso 4: devolver PCB actualizado a Kernel
+
+  // Paso 5: liberar memoria
+  free(pcb);  
+  //Falta liberar/destruir la lista
 
   return NULL;
 }
 
 //Recibir información del PCB desde Kernel
-t_pcb* recibir_pcb(int socket_dispatch) {
+t_pcb* recibir_pcb(int conexion_kernel_dispatch) {
   void* buffer = malloc(sizeof(uint32_t)*2);
 
-  int bytes_recibidos= recv(socket_dispatch,buffer,sizeof(uint32_t)*2,MSG_WAITALL);
+  int bytes_recibidos= recv(conexion_kernel_dispatch,buffer,sizeof(uint32_t)*2,MSG_WAITALL);
   if (bytes_recibidos <= 0) {
     log_info(logger,"Fallo al recibir instrucción");
     free (buffer);
@@ -108,58 +128,83 @@ t_pcb* deserializar_pcb(void* buffer) {
 }
 
 //Solicitar instrucción a Memoria
-void* recibir_instruccion(t_pcb* pcb, int conexion_memoria) {
+t_list* recibir_instruccion(t_pcb* pcb, int conexion_memoria) {
 
   send(conexion_memoria, &(pcb->pc), sizeof(uint32_t), 0);
   send(conexion_memoria, & (pcb->pc), sizeof(uint32_t), 0);
 
-  t_instruccion instruccion_recibida;
-  recv(conexion_memoria, &instruccion_recibida, sizeof(t_instruccion), 0);
+  t_list* lista_instrucciones;
+  lista_instrucciones=recibir_paquete(conexion_memoria);
 
-  return NULL;
+  return lista_instrucciones;
 }
 
-void decode  ( t_instruccion instruccion, uint32_t pid) {
-  swithc (instruccion.tipo) {
+
+//Decode y Execute Instrucción
+t_resultado_ejecucion trabajar_instruccion (t_instruccion instruccion, t_pcb* pcb) {
+  switch (instruccion.tipo) {
+  //El log_info en cada Case corresponde a Instrucción Ejecutada
     case NOOP:
-         log_info (logger, "## PID: %d - Instrucción: NOOP" , pid );
+         log_info (logger, "## PID: %d - Ejecutando: NOOP", pcb->pid);
+         ejecutar_noop();
+         pcb->pc++;
+         return EJECUCION_CONTINUA;
          break;
     
     case READ :
-         log_info(logger, "## PID: %d - Instruccion: READ - Direccion logica: %d - tamaño: %d", pid, instruccion.parametro1, instruccion.parametro2);
+         log_info(logger, "## PID: %d - Ejecutando: READ - Direccion logica: %d - tamaño: %d", pcb->pid, instruccion.parametro1, instruccion.parametro2);
+         ejecutar_read(instruccion.parametro1, instruccion.parametro2);
+         pcb->pc++;
+         return EJECUCION_CONTINUA;
          break;
     
     case WRITE : 
-         log_info(logger, "## PID: %d - Instruccion: WRITE - Direccion logica: %d - Valor: %d ", pid, instruccion.parametro1, instruccion.parametro2);
+         log_info(logger, "## PID: %d - Ejecutando: WRITE - Direccion logica: %d - Valor: %d ", pcb->pid, instruccion.parametro1, instruccion.parametro2);
+         ejecutar_write(instruccion.parametro1, instruccion.parametro2);
+         pcb->pc++;
+         return EJECUCION_CONTINUA;
          break;
     
     case GOTO: 
-         log_info(logger, "## PID: %d - Instruccion: GOTO - Nuevo PC: %d", pid, instruccion.parametro1);
+         log_info(logger, "## PID: %d - Ejecutando: GOTO - Nuevo PC: %d", pcb->pid, instruccion.parametro1);
+         pcb->pc=instruccion.parametro1;
+         return EJECUCION_CONTINUA;
          break;
     
     case IO: 
-         log_info(logger, "## PID: %d - Instruccion: IO - Dispositivo: %d - Tiempo: %d", pid, instruccion.parametro1, instruccion.parametro2);
+         log_info(logger, "## PID: %d - Ejecutando: IO - Dispositivo: %d - Tiempo: %d", pcb->pid, instruccion.parametro1, instruccion.parametro2);
+         ejecutar_io(instruccion.parametro1, instruccion.parametro2);
+         pcb->pc++;
+         return EJECUCION_CONTINUA;
          break;
     
     case INIT_PROC:
-         log_info(logger, "## PID: %d - Instruccion: INIT_PROC - Archivo: %d - Tamaño: %d", pid, instruccion.parametro1, instruccion.parametro2);
+         log_info(logger, "## PID: %d - Ejecutando: INIT_PROC - Archivo: %d - Tamaño: %d", pcb->pid, instruccion.parametro1, instruccion.parametro2);
+         ejecutar_init_proc(instruccion.parametro1, instruccion.parametro2);
+         pcb->pc++;
+         return EJECUCION_CONTINUA;
          break;
 
     case DUMP_MEMORY:
-         log_info(logger,"## PID: %d - Instruccion: DUMP_MEMORY", pid);
+         log_info(logger,"## PID: %d - Ejecutando: DUMP_MEMORY", pcb->pid);
+         ejecutar_dump_memory();
+         pcb->pc++;
+         return EJECUCION_CONTINUA;
          break;
 
     case EXIT:
-         log_info (logger, "## PID: %d- Instruccion: EXIT", pid);
+         log_info (logger, "## PID: %d- Ejecutando: EXIT", pcb->pid);
+         ejecutar_exit();
+         pcb->pc++;
+         return EJECUCION_FINALIZADA;
          break;
     
     default: 
-    log_warning(logger, "## PID: %d - Instruccion desconocida: %d", pid, instruccion.tipo);
+    log_warning(logger, "## PID: %d - Instruccion desconocida: %d", pcb->pid, instruccion.tipo);
      break;
-  
+     return EJECUCION_FINALIZADA;
    }
 }
-
 
 
 
