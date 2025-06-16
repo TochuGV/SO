e6e08ade5330a5306f1556e977b868d4f609471b
FedericoAles <federico.ales29@gmail.com>
2025-06-16 18:51:51 +0000

    Implementar funcion de recibir solicitud de marco

--- memoria/src/atencion_cpu.c
+++ memoria/src/atencion_cpu.c
@@ -6,19 +6,39 @@ void* atender_cpu(void* arg)
 {
   int cliente_cpu = *(int*)arg;
 
-    t_list* lista;
     while (1) {
       int cod_op = recibir_operacion(cliente_cpu);
       //log_debug(logger,"CODOP: %d",cod_op);
       switch (cod_op) {
-      case PAQUETE:
-        lista = recibir_paquete(cliente_cpu);
-        list_iterate(lista, (void*) iterator);
+      
+      case DATOS_MEMORIA:
+        t_paquete* paquete_datos = crear_paquete(DATOS_MEMORIA);
+
+        agregar_a_paquete(paquete_datos, &tamanio_pagina, sizeof(uint32_t));
+        agregar_a_paquete(paquete_datos, &entradas_por_tabla, sizeof(uint32_t));
+        agregar_a_paquete(paquete_datos, &cantidad_niveles, sizeof(uint32_t)); 
+
+        enviar_paquete(paquete_datos, cliente_cpu);
+
         break;
+
       case SOLICITUD_INSTRUCCION:
         //log_debug(logger,"Entre al caso SOLICITUD INSTRUCCION");
         recibir_solicitud_instruccion(cliente_cpu);
         break;
+      
+      case SOLICITUD_MARCO:
+        recibir_solicitud_marco(cliente_cpu);
+        break;
+
+      case ESCRITURA:
+        // Escribir los datos que indique cpu en la direccion fisica que reciba de cpu
+        break;
+
+      case LECTURA:
+        // Leer el tamaño de memoria indicado por cpu en la direccion fisica que reciba de cpu
+        break;
+
       case -1:
         log_error(logger, "CPU se desconectó. Terminando servidor...");
         bool misma_cpu(void* elem)
@@ -85,3 +105,50 @@ void* recibir_solicitud_instruccion(int cliente_cpu)
   return NULL;
 }
 
+void recibir_solicitud_marco(int cliente_cpu)
+{
+  t_list* valores = recibir_paquete(cliente_cpu);
+
+  uint32_t pid = *(uint32_t*)list_get(valores, 0);
+  t_proceso* proceso = obtener_proceso(pid);
+
+  t_tabla* tabla_actual = proceso->tabla_de_paginas;
+  t_entrada* entrada = NULL;
+
+  if (list_size(valores) - 1 != cantidad_niveles) {
+    log_error(logger, "Error al recibir las entradas de nivel del proceso con PID: %d", pid);
+    return;
+  }
+
+  for (int nivel = 1; nivel <= cantidad_niveles; nivel++)
+  {
+    uint32_t entrada_nivel = *(uint32_t*)list_get(valores, nivel);
+
+    entrada = list_get(tabla_actual->entradas, entrada_nivel);
+
+    if (!entrada) {
+      log_error(logger, "Error ecorriendo la tabla de paginas del proceso con PID: %d", pid);
+      return;
+    }
+
+    proceso->metricas[ACCESOS_TABLA_PAGINAS]++;
+    usleep(retardo_memoria * 1000);
+
+    if (nivel != cantidad_niveles){
+      tabla_actual = entrada->siguiente_tabla;
+      if (!tabla_actual) {
+        log_error(logger, "Error ecorriendo la tabla de paginas del proceso con PID: %d", pid);
+        return;
+      }
+    } 
+  }
+
+  uint32_t marco = entrada->marco;
+
+  if (marco == -1)
+    log_warning(logger, "El marco solicitado del proceso con PID: %d, no existe. Se envia el valor -1", pid);
+
+  send(cliente_cpu, &marco, sizeof(uint32_t), 0);
+
+  list_destroy_and_destroy_elements(valores, free);
+}
