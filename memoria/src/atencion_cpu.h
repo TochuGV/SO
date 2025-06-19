9cd40e9c4be1ae089a16e74f78a6c5f36214e767
FedericoAles <federico.ales29@gmail.com>
2025-06-19 02:54:48 +0000

    Actualizar memoria y desarrollar funciones de READ y WRITE

--- memoria/src/atencion_cpu.h
+++ memoria/src/atencion_cpu.h
@@ -20,5 +20,7 @@
 void* atender_cpu(void*);
 void* recibir_solicitud_instruccion(int cliente_cpu);
 void recibir_solicitud_marco(int cliente_cpu);
+void recibir_solicitud_lectura(int cliente_cpu);
+void recibir_solicitud_escritura(int cliente_cpu);
 
 #endif /* ATENCION_CPU_H_ */
