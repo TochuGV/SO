5885d84036977ed0ca9cac8286fdca8dd7dcbd30
TochuGV <tvallejos@frba.utn.edu.ar>
2025-06-28 04:24:08 +0000

    Reutilizar código de los algoritmos de planificación de largo plazo

--- kernel/src/planificacion/planificadores/largo_plazo/algoritmos/fifo/fifo.h
+++ kernel/src/planificacion/planificadores/largo_plazo/algoritmos/fifo/fifo.h
@@ -2,9 +2,8 @@
 #define FIFO_H_
 
 #include "planificacion/planificadores/largo_plazo/largo_plazo.h"
+#include "planificacion/planificadores/largo_plazo/algoritmos/algoritmos.h"
 #include "pcb/pcb.h"
-#include "common/common.h"
-#include "planificacion/estados/estados.h"
 #include <commons/collections/queue.h>
 #include <semaphore.h>
 
