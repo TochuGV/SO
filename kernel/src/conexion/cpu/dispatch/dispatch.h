53a1871ab0ade959bfad2981089776bb0c199b38
TochuGV <tvallejos@frba.utn.edu.ar>
2025-07-23 02:16:46 +0000

    Permitir reconexión de CPU al Kernel tras desconexión

--- kernel/src/conexion/cpu/dispatch/dispatch.h
+++ kernel/src/conexion/cpu/dispatch/dispatch.h
@@ -7,6 +7,7 @@
 #include "syscalls/syscalls.h"
 #include "pcb/pcb.h"
 #include "cpu/cpu.h"
+#include "conexion/cpu/desconexion/desconexion.h"
 
 void* conectar_cpu_dispatch(void*);
 void* atender_cpu_dispatch(void*);
